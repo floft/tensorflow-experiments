dbf6204dc95b49b26360ed6efaed3c29141f5632
Garrett Wilson <g@floft.net>
2018-09-17 09:16:04 -0700

    Generate dataset with different y-intercept
    Also set numpy random seed for reproducability

--- generate_trivial_datasets.py
+++ generate_trivial_datasets.py
@@ -20,10 +20,11 @@ def linear(m, b, length=100, minvalue=0, maxvalue=100):
 def is_positive_slope(m):
     return m > 0
 
-def generate_positive_slope_data(n, display=False, add_noise=False):
+def generate_positive_slope_data(n, display=False, add_noise=False,
+        bmin=0, bmax=100, m_mu=0, m_std=1):
     """ Positive or negative slope lines """
-    m = np.random.normal(0, 1, (1,n))
-    b = np.random.randint(-100, 100, (1,n))
+    m = np.random.normal(m_mu, m_std, (1,n))
+    b = np.random.randint(bmin, bmax, (1,n))
     x, y = linear(m, b)
     labels = is_positive_slope(m)
 
@@ -41,16 +42,17 @@ def generate_positive_slope_data(n, display=False, add_noise=False):
     df.insert(0, 'class', pd.Series(np.squeeze(labels).astype(np.int32)+1, index=df.index))
     return df
 
-def sine(m, b, length=100, minvalue=0, maxvalue=100):
+def sine(m, b, length=100, minvalue=0, maxvalue=100, horiz_scale=10):
     x = np.arange(minvalue, maxvalue, (maxvalue-minvalue)/length).reshape(-1,1)
-    y = m*np.sin(x/10) + b # x/10 to make adjust horizontal scale
+    y = m*np.sin(1.0*x/horiz_scale) + b
     return x, y
 
-def generate_positive_sine_data(n, display=False, add_noise=False):
+def generate_positive_sine_data(n, display=False, add_noise=False,
+        bmin=0, bmax=100, m_mu=0, m_std=10, horiz_scale=10):
     """ Sine wave multiplied by positive or negative number and offset some """
-    m = np.random.normal(0, 10, (1,n))
-    b = np.random.randint(-100, 100, (1,n))
-    x, y = sine(m, b)
+    m = np.random.normal(m_mu, m_std, (1,n))
+    b = np.random.randint(bmin, bmax, (1,n))
+    x, y = sine(m, b, horiz_scale=horiz_scale)
     labels = is_positive_slope(m)
 
     if add_noise:
@@ -67,18 +69,26 @@ def generate_positive_sine_data(n, display=False, add_noise=False):
     df.insert(0, 'class', pd.Series(np.squeeze(labels).astype(np.int32)+1, index=df.index))
     return df
 
+def save_data(func, fn):
+    """ Use func to create examples that are saved to fn_TRAIN and fn_TEST """
+    func(1000).to_csv('trivial/'+fn+'_TRAIN', header=False, index=False)
+    func(100).to_csv('trivial/'+fn+'_TEST', header=False, index=False)
+
 if __name__ == '__main__':
+    # For reproducability
+    np.random.seed(0)
+
     if not os.path.exists('trivial'):
         os.makedirs('trivial')
 
     # No noise
-    generate_positive_slope_data(1000).to_csv('trivial/positive_slope_TRAIN', header=False, index=False)
-    generate_positive_slope_data(100).to_csv('trivial/positive_slope_TEST', header=False, index=False)
-    generate_positive_sine_data(1000).to_csv('trivial/positive_sine_TRAIN', header=False, index=False)
-    generate_positive_sine_data(100).to_csv('trivial/positive_sine_TEST', header=False, index=False)
+    save_data(generate_positive_slope_data, 'positive_slope')
+    save_data(generate_positive_sine_data, 'positive_sine')
 
     # Noisy
-    generate_positive_slope_data(1000, add_noise=True).to_csv('trivial/positive_slope_noise_TRAIN', header=False, index=False)
-    generate_positive_slope_data(100, add_noise=True).to_csv('trivial/positive_slope_noise_TEST', header=False, index=False)
-    generate_positive_sine_data(1000, add_noise=True).to_csv('trivial/positive_sine_noise_TRAIN', header=False, index=False)
-    generate_positive_sine_data(100, add_noise=True).to_csv('trivial/positive_sine_noise_TEST', header=False, index=False)
+    save_data(lambda x: generate_positive_slope_data(x, add_noise=True), 'positive_slope_noise')
+    save_data(lambda x: generate_positive_sine_data(x, add_noise=True), 'positive_sine_noise')
+
+    # No noise - but different y-intercept
+    save_data(lambda x: generate_positive_slope_data(x, bmin=-200, bmax=-100), 'positive_slope_low')
+    save_data(lambda x: generate_positive_sine_data(x, bmin=-200, bmax=-100), 'positive_sine_low')
