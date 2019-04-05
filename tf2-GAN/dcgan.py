18c7f20982be66e68f0cb9f1b93957690cdca666
Garrett Wilson <g@floft.net>
2019-04-05 13:11:30 -0700

    Revert "wgan attempt"
    This reverts commit b42a506b4f4d956610dee9328e83027d305b738a.

--- tf2-GAN/dcgan.py
+++ tf2-GAN/dcgan.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 from IPython import display
 from tensorflow.keras import layers
 
-def load_mnist(buffer_size=6000, batch_size=32, prefetch_buffer_size=1):
+def load_mnist(buffer_size=60000, batch_size=256, prefetch_buffer_size=1):
     # Load
     (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
     # Reshape to (# examples, 28, 28, 1) and set to float32
@@ -25,66 +25,52 @@ def load_mnist(buffer_size=6000, batch_size=32, prefetch_buffer_size=1):
     train_images = (train_images - 127.5) / 127.5
     # Shuffle and batch
     train_dataset = tf.data.Dataset.from_tensor_slices(train_images).\
-        repeat().shuffle(buffer_size).batch(batch_size).\
-        prefetch(prefetch_buffer_size)
+        shuffle(buffer_size).batch(batch_size).prefetch(prefetch_buffer_size)
 
     return train_dataset
 
-def make_generator_model(noise_dim, weight_decay=0.001):
-    model = tf.keras.Sequential([
-        layers.Dense(1024, use_bias=False, input_shape=(noise_dim,),
-            kernel_regularizer=tf.keras.regularizers.l2(weight_decay)),
-        layers.BatchNormalization(),
-        layers.Activation("relu"),
+# TODO implement these in tf.keras.Sequential([ ... ]) instead
+def make_generator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
 
-        layers.Dense(7*7*256, use_bias=False,
-            kernel_regularizer=tf.keras.regularizers.l2(weight_decay)),
-        layers.BatchNormalization(),
-        layers.Activation("relu"),
+    model.add(layers.Reshape((7, 7, 256)))
+    assert model.output_shape == (None, 7, 7, 256) # Note: None is the batch size
 
-        layers.Reshape((7, 7, 256)),
-        layers.Conv2DTranspose(64, (4, 4), strides=(2, 2), padding='same', use_bias=False,
-            kernel_regularizer=tf.keras.regularizers.l2(weight_decay)),
-        layers.BatchNormalization(),
-        layers.Activation("relu"),
+    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
 
-        layers.Conv2DTranspose(32, (4, 4), strides=(2, 2), padding='same', use_bias=False,
-            kernel_regularizer=tf.keras.regularizers.l2(weight_decay)),
-        layers.BatchNormalization(),
-        layers.Activation("relu"),
+    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
 
-        #layers.Conv2D(1, (4, 4), strides=(2, 2), padding='same', use_bias=False, activation='tanh'),
-        layers.Conv2D(1, (4, 4), strides=(1, 1), padding='same', use_bias=False, activation='tanh'),
-    ])
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
     assert model.output_shape == (None, 28, 28, 1)
+
     return model
 
-def make_discriminator_model(weight_decay=0.001):
-    return tf.keras.Sequential([
-        layers.Conv2D(64, (4, 4), strides=(2, 2), padding='same', input_shape=[28, 28, 1],
-            kernel_regularizer=tf.keras.regularizers.l2(weight_decay),
-            bias_regularizer=tf.keras.regularizers.l2(weight_decay)),
-        layers.LeakyReLU(),
-        layers.Dropout(0.3),
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                                     input_shape=[28, 28, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
 
-        layers.Conv2D(128, (4, 4), strides=(2, 2), padding='same',
-            kernel_regularizer=tf.keras.regularizers.l2(weight_decay),
-            bias_regularizer=tf.keras.regularizers.l2(weight_decay)),
-        layers.LeakyReLU(),
-        layers.Dropout(0.3),
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
 
-        layers.Flatten(),
-        layers.Dense(1024,
-            kernel_regularizer=tf.keras.regularizers.l2(weight_decay),
-            bias_regularizer=tf.keras.regularizers.l2(weight_decay)),
-        layers.BatchNormalization(),
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
 
-        layers.Dense(1,
-            kernel_regularizer=tf.keras.regularizers.l2(weight_decay),
-            bias_regularizer=tf.keras.regularizers.l2(weight_decay)),
-    ])
+    return model
 
-def make_losses(wgan=False):
+def make_losses():
     cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
     def discriminator_loss(real_output, fake_output):
@@ -96,23 +82,14 @@ def make_losses(wgan=False):
     def generator_loss(fake_output):
         return cross_entropy(tf.ones_like(fake_output), fake_output)
 
-    def wgan_discriminator_loss(real_output, fake_output):
-        return tf.reduce_mean(fake_output) - tf.reduce_mean(real_output)
-
-    def wgan_generator_loss(fake_output):
-        return -tf.reduce_mean(fake_output)
-
-    if wgan:
-        return wgan_discriminator_loss, wgan_generator_loss
-    else:
-        return discriminator_loss, generator_loss
+    return discriminator_loss, generator_loss
 
 # TODO use two separate networks sharing weights and use train_on_batch?
 # and test_on_batch?
 @tf.function
 def train_step(images, generator, discriminator, generator_loss,
         discriminator_loss, generator_optimizer, discriminator_optimizer,
-        noise_dim, wgan):
+        noise_dim):
     batch_size = images.shape[0]
     noise = tf.random.normal([batch_size, noise_dim])
 
@@ -131,36 +108,32 @@ def train_step(images, generator, discriminator, generator_loss,
     generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
     discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
 
-    # Clip discriminator weights if using WGAN loss
-    if wgan:
-        for w in discriminator.trainable_variables:
-            if "kernel" in w.name or "bias" in w.name:
-                w.assign(tf.clip_by_value(w, -0.01, 0.01))
-                print("Will clip", w.name)
-
 def train(dataset, generator, discriminator, generator_loss, discriminator_loss,
-        generator_optimizer, discriminator_optimizer, wgan, noise_dim, seed,
-        steps, checkpoint_manager):
-    dataset_iter = iter(dataset)
-
-    for step in range(steps):
+        generator_optimizer, discriminator_optimizer, noise_dim, seed, epochs,
+        checkpoint_manager):
+    for epoch in range(epochs):
         start = time.time()
-        image_batch = next(dataset_iter)
 
-        train_step(image_batch, generator, discriminator, generator_loss,
+        for image_batch in dataset:
+            train_step(image_batch, generator, discriminator, generator_loss,
                 discriminator_loss, generator_optimizer,
-                discriminator_optimizer, noise_dim, wgan)
+                discriminator_optimizer, noise_dim)
 
-        if (step+1)%100 == 0:
-            display.clear_output(wait=True)
-            generate_and_save_images(generator, step+1, seed)
+        # Produce images for the GIF as we go
+        display.clear_output(wait=True)
+        generate_and_save_images(generator, epoch+1, seed)
 
-        if (step+1)%400 == 0:
-            checkpoint_manager.save(checkpoint_number=step+1)
+        # Save the model every 5 epochs
+        if (epoch+1)%5 == 0:
+            checkpoint_manager.save(checkpoint_number=epoch+1)
 
-        print("Time for step {} is {} sec".format(step+1, time.time()-start))
+        print('Time for epoch {} is {} sec'.format(epoch+1, time.time()-start))
 
-def generate_and_save_images(model, step, test_input):
+    # Generate after the final epoch
+    display.clear_output(wait=True)
+    generate_and_save_images(generator, epochs, seed)
+
+def generate_and_save_images(model, epoch, test_input):
     predictions = model(test_input, training=False)
 
     plt.ion()
@@ -171,27 +144,20 @@ def generate_and_save_images(model, step, test_input):
         plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
         plt.axis('off')
 
-    plt.savefig('image_at_step_{:04d}.png'.format(step))
+    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
     plt.show()
     fig.canvas.flush_events()
 
 if __name__ == "__main__":
-    # Params
-    wgan = True
-    steps = 1200
-    noise_dim = 50
-    num_examples_to_generate = 16
-    seed = tf.random.normal([num_examples_to_generate, noise_dim])
-
     # Load training data
     train_dataset = load_mnist()
 
     # Create models, losses, optimizers
-    generator = make_generator_model(noise_dim)
+    generator = make_generator_model()
     discriminator = make_discriminator_model()
-    discriminator_loss, generator_loss = make_losses(wgan)
-    generator_optimizer = tf.keras.optimizers.Adam(1e-3, beta_1=0.5)
-    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4, beta_1=0.5)
+    discriminator_loss, generator_loss = make_losses()
+    generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 
     # Allow loading the model again after training
     checkpoint = tf.train.Checkpoint(
@@ -199,10 +165,15 @@ if __name__ == "__main__":
         discriminator_optimizer=discriminator_optimizer,
         generator=generator, discriminator=discriminator)
     checkpoint_manager = tf.train.CheckpointManager(checkpoint,
-        directory="./training_checkpoints", max_to_keep=1)
+        directory="./training_checkpoints", max_to_keep=5)
     #checkpoint.restore(checkpoint_manager.latest_checkpoint)
 
     # Train
+    epochs = 150
+    noise_dim = 100
+    num_examples_to_generate = 16
+    seed = tf.random.normal([num_examples_to_generate, noise_dim])
+
     train(train_dataset, generator, discriminator, generator_loss,
         discriminator_loss, generator_optimizer, discriminator_optimizer,
-        wgan, noise_dim, seed, steps, checkpoint_manager)
+        noise_dim, seed, epochs, checkpoint_manager)
